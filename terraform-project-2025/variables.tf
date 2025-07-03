379e7c7459b4330c45c911d84bfb01bb78465644
Ritesh Mehar <riteshmehar397@gmail.com>
2025-07-03 15:23:21 +0530

    Create variables.tf

---
+++ terraform-project-2025/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+        default = "us-east-1"
+}
+variable "cidr" {
+        default = "10.0.0.0/16"
+}
+variable "bucketname" {
+        default = "terraformprojectbucket2025"
+}
